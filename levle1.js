a9cd2430efe99a3d1fc0cb1d578bb511c9a57f80
Dajhin12 <ashtonfoster2000@gmail.com>
2015-11-10 16:23:00 -0800

    flasheywall

--- levle1.js
+++ levle1.js
@@ -5,7 +5,7 @@ function preload() {
     game.load.image('cube', 'cubes/playercube.png');
     game.stage.backgroundColor = '#C0C0C0';
 
-   game.load.image('WpartG', 'cubes/wallcube(green).png',96,96);   
+   game.load.spritesheet('WpartG', 'cubes/wallcube(green).png',96,96);   
 
     
 }// end preload function
@@ -13,20 +13,24 @@ function preload() {
 var player;
 var walls;
 var cursors;
+var wallGroup;
 function create() {
     player = game.add.sprite(0, 0, 'cube');
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
     
-game.physics.arcade.enable(player);
-    
-player.body.collideWorldBounds = true;    
-cursors = game.input.keyboard.createCursorKeys();    
-walls = game.add.group();   
-walls.enableBody = true;
-    
- walls.create(400, 400, 'WpartG');   
-    
+    game.physics.arcade.enable(player);
+
+    player.body.collideWorldBounds = true;    
+    cursors = game.input.keyboard.createCursorKeys();    
+    walls = game.add.sprite(50,0, 'WpartG');  
+    walls.enableBody = true;
+
+    // walls.create(400, 400, 'WpartG');   
+    walls.animations.add('flashG', [0,1,0], 10, true);
+    walls.animations.play('flashG');
+    wallGroup = game.add.group();
+
     
 
 
