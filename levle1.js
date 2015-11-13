06a10f339e9478d1c600f16f7143620f9e7411d7
Dajhin12 <ashtonfoster2000@gmail.com>
2015-11-12 18:05:54 -0800

    stuffs

--- levle1.js
+++ levle1.js
@@ -5,7 +5,7 @@ function preload() {
     game.load.image('cube', 'cubes/playercube.png');
     game.stage.backgroundColor = '#C0C0C0';
 
-   game.load.spritesheet('WpartG', 'cubes/wallcube(green).png',96,96);   
+   game.load.spritesheet('WpartG', 'cubes/wallcube(green).png',48,48);   
 
     
 }// end preload function
@@ -14,10 +14,16 @@ var player;
 var walls;
 var cursors;
 var wallGroup;
+var map;
+var walls2;
 function create() {
+    
+    
+    game.physics.startSystem(Phaser.Physics.ARCADE) 
+    
     player = game.add.sprite(48, 48, 'cube');
     //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
     
     game.physics.arcade.enable(player);
     
@@ -28,32 +34,39 @@ function create() {
 
     player.body.collideWorldBounds = true;    
     cursors = game.input.keyboard.createCursorKeys(); 
+  
+    map=game.add.tilemap();
+    map.addTilesetImage('WpartG',null,48,48);
+    walls2=map.create('wallkey',12,12,96,96);
+    map.putTile(0,5,5,walls2);
+    map.setCollision(0);
     
-    for(var wall=0; wall<=10; wall++){
-     walls = game.add.sprite(48*wall,48, 'WpartG');
     
-    
-//    game.add.sprite(48,48, 'WpartG'); 
-//    game.add.sprite(96,48, 'WpartG'); 
-//    game.add.sprite(144,48, 'WpartG'); 
-    walls.enableBody = true;
-
-    // walls.create(400, 400, 'WpartG');   
-    walls.animations.add('flashG', [0,1], 4, true);
-    walls.animations.play('flashG');
-    wallGroup = game.add.group();
-     
-        game.physics.arcade.enable(walls);
-        walls.body.immovable = true;
-        
-    }
+//    for(var wall=0; wall<=10; wall++){
+//     walls = game.add.sprite(48*wall,0, 'WpartG');
+//    
+//    
+////    game.add.sprite(48,48, 'WpartG'); 
+////    game.add.sprite(96,48, 'WpartG'); 
+////    game.add.sprite(144,48, 'WpartG'); 
+//    walls.enableBody = true;
+//
+//    // walls.create(400, 400, 'WpartG');   
+//    walls.animations.add('flashG', [0,1], 4, true);
+//    walls.animations.play('flashG');
+//    wallGroup = game.add.group();
+//     
+//        game.physics.arcade.enable(walls);
+////        walls.body.immovable = true;
+//        
+//    }
     
 
 } // end create function
 
 function update() {
     
-    game.physics.arcade.collide(walls,player);
+    game.physics.arcade.collide(player,walls2);
     
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
